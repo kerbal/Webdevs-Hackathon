8265ba2a7bc114a7404a10d3a35532a8d97ecaa0
Phạm Dũng <tiendung1510@outlook.com.vn>
2019-03-28 22:08:53 +0700

    update layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, Switch } from "react-router-dom";
+import {  Switch } from "react-router-dom";
 import { Header } from './components/Header';
 import { PrivateRoute } from './containers/CustomRoute';
 import { Home } from './routes/Home';
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,22 +1,25 @@
 import { Subject } from "../utils/Observable";
+import { history } from '..';
 
 export class AuthService {
   static $auth = new Subject();
 
   static user = sessionStorage.getItem('wdh_js_user');
 
-  static logout(history) {
+  static logout() {
+    console.log('logout');
     AuthService.user = null;
     sessionStorage.removeItem('wdh_js_user');
     AuthService.$auth.broadcast(AuthService.logged);
-    return true;
+    history.push('/');
   }
 
   static login() {
+    console.log('login')
     AuthService.user = "admin";
     sessionStorage.setItem('wdh_js_user', 'admin');
     AuthService.$auth.broadcast(AuthService.logged);
-    return true;
+    history.push('/app');
   }
 
   static get logged() {
